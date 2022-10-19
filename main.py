499a26595ade06095c13b8e189dab33d6275ef41
SenneDeWinter <senne.dewinter@student.kdg.be>
2022-10-19 10:26:54 +0200

    Created functions & added code from other .py files

--- main.py
+++ main.py
@@ -10,3 +10,55 @@ import de2120_barcode_scanner
 
 #Other
 import time
+
+def main():
+    check_database()
+
+def read_barcode():
+    my_scanner = de2120_barcode_scanner.DE2120BarcodeScanner()
+
+
+    scan_buffer = ""
+    barcode = ""
+
+    while True:
+        scan_buffer = my_scanner.read_barcode()
+        if scan_buffer:
+            barcode = str(scan_buffer)
+            print(barcode)
+            scan_buffer = ""
+
+        time.sleep(0.02)
+
+def check_database():
+    mydb = mysql.connector.connect(
+    host = secrets.host,
+    user = secrets.user,
+    password = secrets.password,
+    database = secrets.database 
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("SELECT * FROM parcels")
+
+myresult = mycursor.fetchall()
+
+print(myresult)
+
+def control_lock():
+    relay = OutputDevice(16)
+
+led = RGBLED(26, 19, 13)
+
+while True:
+    relay.on()
+    led.color = (0,1,1)
+    time.sleep(5)
+    relay.off()
+    led.color = (1,1,0)
+    time.sleep(5) 
+
+
+if __name__ == '__main__':
+    main()
