bc1cede1d509a43e8ecfad40c02d2f813d50e84f
SenneDeWinter <senne.dewinter@student.kdg.be>
2022-10-19 10:09:09 +0200

    Added RGB status LED

--- relay.py
+++ relay.py
@@ -1,11 +1,15 @@
 from ast import While
-from gpiozero import OutputDevice
+from gpiozero import OutputDevice, RGBLED
 import time
 
 relay = OutputDevice(16)
 
+led = RGBLED(26, 19, 13)
+
 while True:
     relay.on()
+    led.color = (0,1,1)
     time.sleep(5)
     relay.off()
+    led.color = (1,1,0)
     time.sleep(5) 
